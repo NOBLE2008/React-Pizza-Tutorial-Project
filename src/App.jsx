e5bb4b07e68942a7d28ae385f370301f3756a481
Noble Benjamin <nobleben2008@gmail.com>
2024-06-03 22:46:12 +0100

    Made First Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from 'react'
 
+const Pizza = () => {
+  return (
+    <div>
+      <h2>Pizza</h2>
+    </div>
+  )
+}
  function App(){
   
   const [time, setTime] = useState(new Date().toLocaleTimeString())
@@ -9,9 +16,10 @@ import { useEffect, useState } from 'react'
       setTime(new Date().toLocaleTimeString())
     }, 1000)
   }, [])
-
+  
   return <>
     <h1>Hello World</h1>
+    <Pizza/>
   </>
  }
 
