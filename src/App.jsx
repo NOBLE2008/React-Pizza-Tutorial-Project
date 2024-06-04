6f231d90d17e5819eeced945726fb4f86410b840
Noble Benjamin <nobleben2008@gmail.com>
2024-06-04 21:19:46 +0100

    Moved Menu Into a component folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 const Header = () => {
-  return (
-    <h1>Fast React Pizza Co.</h1>
-  )
+
 }
 
 const Footer  = () => {
   return (
     <h3>Copyright {new Date().getFullYear()}</h3>
   )
-}
-
-const Menu = () => {
-  return <>
-    <div>
-      <img src="pizzas/focaccia.jpg" alt="pizza1" />
-      <img src="pizzas/funghi.jpg" alt="" />
-      <img src="pizzas/margherita.jpg" alt="" />
-      <img src="pizzas/prosciutto.jpg" alt="" />
-      <img src="pizzas/salamino.jpg" alt="" />
-      <img src="pizzas/spinaci.jpg" alt="" />
-    </div>
-  </>
 }
 
  function App(){
