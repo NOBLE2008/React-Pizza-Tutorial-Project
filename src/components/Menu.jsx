a7f5b9da68630390d9ec7649199943712ae3a2ea
Noble Benjamin <nobleben2008@gmail.com>
2024-06-05 20:39:56 +0100

    Styled Pizza Text in the UI

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,23 +6,23 @@ const Menu = () => {
     <div className="pizza-con">
     <div className='pizza'>
         <div><img src="pizzas/funghi.jpg" alt="" /></div>
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+        <div className='text-con'><div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div></div>
     </div>
     <div className='pizza'>
         <div><img src="pizzas/funghi.jpg" alt="" /></div>
-        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
+        <div className='text-con'><div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div></div>
     </div>
     <div className='pizza'>
         <div><img src="pizzas/funghi.jpg" alt="" /></div>
-        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
+        <div className='text-con'><div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div></div>
     </div>
     <div className='pizza'>
         <div><img src="pizzas/funghi.jpg" alt="" /></div>
-        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
+        <div className='text-con'><div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div></div>
     </div>
     <div className='pizza'>
         <div><img src="pizzas/funghi.jpg" alt="" /></div>
-        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
+        <div className='text-con'><div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div></div>
     </div>
     </div>
     </div>
