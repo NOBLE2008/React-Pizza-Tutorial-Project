06004d7d5525b6a0cc0c288f1fff2d4309fdec1d
Noble Benjamin <nobleben2008@gmail.com>
2024-06-05 20:28:43 +0100

    Made Custom Hard Coded Pizzas in the Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,26 @@ const Menu = () => {
     return <>
     <div className="contain">
     <div className="pizza-con">
-
+    <div className='pizza'>
+        <div><img src="pizzas/funghi.jpg" alt="" /></div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+    </div>
+    <div className='pizza'>
+        <div><img src="pizzas/funghi.jpg" alt="" /></div>
+        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
+    </div>
+    <div className='pizza'>
+        <div><img src="pizzas/funghi.jpg" alt="" /></div>
+        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
+    </div>
+    <div className='pizza'>
+        <div><img src="pizzas/funghi.jpg" alt="" /></div>
+        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
+    </div>
+    <div className='pizza'>
+        <div><img src="pizzas/funghi.jpg" alt="" /></div>
+        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
+    </div>
     </div>
     </div>
     </>
